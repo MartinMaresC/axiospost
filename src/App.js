aeb10541001bd3bf1baf656bd4ec0168f154a250
Martin Mares <78748150+MartinMaresC@users.noreply.github.com>
2024-06-03 00:56:53 -0600

    Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState([]);
+  useEffect(()=>{
+    axios.post('https://reqres.in/api/users', 
+    {
+      "id": "123",
+      "name": "Martin",
+      "job": "Developer"
+  }
+  )
+  .then(res => console.log(res))
+  .catch(err=> console.log(err))
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Axios Library in React JS</h2>
+      {/*
+        data.map((d,i)=>{
+          return <p key = {i}>{d.email}</p>
+        })
+      */}
     </div>
   );
 }
